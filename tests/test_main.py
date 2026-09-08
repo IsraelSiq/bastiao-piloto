024b4d063ffe7985ad6985150102823cfda57d06
Israel Siqueira <96914191+IsraelSiq@users.noreply.github.com>
2026-09-08 00:14:08 -0300

    Add tests, gitignore and Dockerfile

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    data = response.json()
+    assert "message" in data
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    data = response.json()
+    assert data == {"status": "ok"}
